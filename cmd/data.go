e8dcba025e012a3606afcd0caae24112ebf6ab1c
ebitezion <ebitezion@gmail.com>
2021-07-15 05:36:51 +0100

    added dummy data

--- cmd/data.go
+++ cmd/data.go
@@ -27,6 +27,19 @@ func GetItem(id int) item {
 /////////////////////////////////////////////////////////////////////////////
 //DB helper func
 ////////////////////////////////////////////////////////////////////////////
+//dummy data
+var dummy = []map[string]item{
+	{"1": {1, "my grocery task", "purchase at dollar store ok", "12-03-21"}},
+	{"2": {2, "my reading task", "Read the bible back to back", "12-03-21"}},
+	{"3": {3, "my work task", "Programme the app mehn	", "12-03-21"}},
+}
+
+/*var dummy2 = items{
+	[
+		{1, "my grocery task", "purchase at dollar store ok", "12-03-21"},
+	]
+}*/
+
 func DBGet(id int) (item item, err error) {
 	//get item from db based on id
 
